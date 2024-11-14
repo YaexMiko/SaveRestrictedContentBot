b2b2d855eb7736178c401ea5238aca2d7fc4351b
YaexMiko <aloksinghmains756@gmail.com>
2024-11-14 21:34:30 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("API_ID", "27692015")
+API_HASH = config("API_HASH", "25278a8394b5914ee1b8d6a6c79d572d")
+BOT_TOKEN = config("BOT_TOKEN", "7716433955:AAFyRm41MWU-K0MslxcRWvE5tOYFMCS3pO8")
+SESSION = config("SESSION", "BQGmi-8Arhpi93k8Wiu0OXxhtgevxtH6iVAm7f3fZPiCKXZphoes5UeXwl4dvU-l_ph5Fk3WtcvgVHPA_Xmdv2JYnKxkEEyL40Sjlxnd2wuB13pJNSmC_I4t0WzLAOnqt804pKw8Tudfp03aPD-KFRURp8HZ71KDChX0Ou3RHca7wkazr8HKhRRIudb4hl80OWTUnfToDKOU_2egdIQQYBsnea1yC2JgEsJL6yY7EVgFcAVn0a_sAUwCYpOmyCnk9teaCPLYxGMM21HF5GuRU751Dpsr9MHzgNB8Xu00YSbgIv7qOQSMBVOhG96Du_8VtgVAZNjEzId1vlD0-8y5jX6k6vnAAAAAGmvyK6AA")
+FORCESUB = config("FORCESUB", "")
+AUTH = config("AUTH", "-10020191863748108281129")
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
